ccc764fb7f026da7c156042131dc12979ae3f9ac
muzzyxd <sheikhmuzammil676@gmail.com>
2024-03-12 17:05:33 +0500

    create a mercedes web

---
+++ layout.js
@@ -0,0 +1,37 @@
+document.getElementById("nav").innerHTML=
+`<nav class="navbar navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+      <a href="layout.html"><img src="Mercedes-Logo.svg.png" alt="" height="70px" width="70px"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link"  href="layout.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Vehicles
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="MerBenzGClass.html">Mercedes-Benz</a></li>
+              <li><a class="dropdown-item" href="#">Mercedes-AMG</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="aboutus.html">About Us</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-secondary" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+    </nav>`
